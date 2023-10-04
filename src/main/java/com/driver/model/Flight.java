0c9a9a41314d130f89f9dfb3a099f9df1ab42f00
Ashwitha-Reddy-K <ashwithareddykankanala@gmail.com>
2023-10-04 09:56:24 -0500

    my ease trip project completed

--- src/main/java/com/driver/model/Flight.java
+++ src/main/java/com/driver/model/Flight.java
@@ -2,79 +2,82 @@ package com.driver.model;
 
 import java.util.Date;
 
-public class Flight {
 
-    private int flightId; //This is a unique key for a flight
+    public class Flight {
 
-    private City fromCity;
+        private int flightId; //This is a unique key for a flight
 
-    private City toCity;
+        private City fromCity;
 
-    private int maxCapacity;
+        private City toCity;
 
-    private Date flightDate;
+        private int maxCapacity;
 
-    private double duration;
+        private Date flightDate;
 
-    public Flight() {
+        private double duration;
+
+
+
+        public Flight() {
+
+        }
+
+        public Flight(int flightId, City fromCity, City toCity, int maxCapacity, Date flightDate, double duration) {
+            this.flightId = flightId;
+            this.fromCity = fromCity;
+            this.toCity = toCity;
+            this.maxCapacity = maxCapacity;
+            this.flightDate = flightDate;
+            this.duration = duration;
+        }
+
+        public int getFlightId() {
+            return flightId;
+        }
+
+        public void setFlightId(int flightId) {
+            this.flightId = flightId;
+        }
+
+        public City getFromCity() {
+            return fromCity;
+        }
+
+        public void setFromCity(City fromCity) {
+            this.fromCity = fromCity;
+        }
+
+        public City getToCity() {
+            return toCity;
+        }
+
+        public void setToCity(City toCity) {
+            this.toCity = toCity;
+        }
+
+        public int getMaxCapacity() {
+            return maxCapacity;
+        }
+
+        public void setMaxCapacity(int maxCapacity) {
+            this.maxCapacity = maxCapacity;
+        }
+
+        public Date getFlightDate() {
+            return flightDate;
+        }
+
+        public void setFlightDate(Date flightDate) {
+            this.flightDate = flightDate;
+        }
+
+        public double getDuration() {
+            return duration;
+        }
+
+        public void setDuration(double duration) {
+            this.duration = duration;
+        }
 
     }
-
-    public Flight(int flightId, City fromCity, City toCity, int maxCapacity, Date flightDate, double duration) {
-        this.flightId = flightId;
-        this.fromCity = fromCity;
-        this.toCity = toCity;
-        this.maxCapacity = maxCapacity;
-        this.flightDate = flightDate;
-        this.duration = duration;
-    }
-
-    public int getFlightId() {
-        return flightId;
-    }
-
-    public void setFlightId(int flightId) {
-        this.flightId = flightId;
-    }
-
-    public City getFromCity() {
-        return fromCity;
-    }
-
-    public void setFromCity(City fromCity) {
-        this.fromCity = fromCity;
-    }
-
-    public City getToCity() {
-        return toCity;
-    }
-
-    public void setToCity(City toCity) {
-        this.toCity = toCity;
-    }
-
-    public int getMaxCapacity() {
-        return maxCapacity;
-    }
-
-    public void setMaxCapacity(int maxCapacity) {
-        this.maxCapacity = maxCapacity;
-    }
-
-    public Date getFlightDate() {
-        return flightDate;
-    }
-
-    public void setFlightDate(Date flightDate) {
-        this.flightDate = flightDate;
-    }
-
-    public double getDuration() {
-        return duration;
-    }
-
-    public void setDuration(double duration) {
-        this.duration = duration;
-    }
-
-}
